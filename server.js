18795fdd2dad8bc932f265f68f3b8ffa2a7bd564
Manuel Castro <maacaro@gmail.com>
2021-09-05 12:56:04 -0400

    add cors config

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const {
   createProxyMiddleware,
   responseInterceptor,
 } = require("http-proxy-middleware");
 const { v4: uuidv4 } = require("uuid");
 var session = require("express-session");
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+var corsOptions = {
+  origin: process.env.APP_URL,
+  optionsSuccessStatus: 200,
+};
 
-app.get("/", (req, res) => {
-  res.send("you are hiting the starting path");
-});
+app.use(cors(corsOptions));
 app.use(
   session({
     genid: (req) => {
@@ -56,7 +60,9 @@ app.use(
     },
   })
 );
-
+app.get("/", (req, res) => {
+  res.send("you are hiting the starting path");
+});
 app.listen(process.env.PORT, () => {
   console.log(`sever listening at ${process.env.PROXY_URL}:${PORT}`);
 });
